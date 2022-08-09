dd38fc95f240745508dbb4ac2531dff2f03adfab
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-08-09 14:06:26 +0300

    BookStore

---
+++ src/test/java/com/ibrahimbayramli/BookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimbayramli.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

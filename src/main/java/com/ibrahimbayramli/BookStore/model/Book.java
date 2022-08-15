a0b572f87cdcdc8706a0f3be290a2722ffff5823
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-08-16 00:48:28 +0300

    BookStore

--- src/main/java/com/ibrahimbayramli/BookStore/model/Book.java
+++ src/main/java/com/ibrahimbayramli/BookStore/model/Book.java
@@ -23,7 +23,7 @@ public class Book {
     @Column(name = "author",nullable = false,length = 100)
     private String author;
 
-    @Column(name="price",nullable = false,)
+    @Column(name="price",nullable = false)
     private Double price;
 
     @Column(name="create_time",nullable = false)

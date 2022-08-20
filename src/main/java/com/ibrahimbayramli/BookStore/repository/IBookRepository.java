e9fc2c91fe734774b148fd066cc2a156b25165ed
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-08-20 15:31:28 +0300

    BookStore

--- src/main/java/com/ibrahimbayramli/BookStore/repository/IBookRepository.java
+++ src/main/java/com/ibrahimbayramli/BookStore/repository/IBookRepository.java
@@ -2,7 +2,9 @@ package com.ibrahimbayramli.BookStore.repository;
 
 import com.ibrahimbayramli.BookStore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IBookRepository extends JpaRepository<Book, Long> {
 
 
--- src/main/java/com/ibrahimbayramli/BookStore/repository/IUserRepository.java
+++ src/main/java/com/ibrahimbayramli/BookStore/repository/IUserRepository.java
@@ -6,16 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+//@Repository
 public interface IUserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByUsername(String username);
 
     @Modifying
-    @Query("update User set role= :role where username= :usename")
-    void updateUserRole(@Param("username") String username, @Param("role")Role role);
+    @Query("update User set role= :role where username= :username")
+    void updateUserRole(@Param("username") String username, @Param("role") Role role);
 
 
 }
